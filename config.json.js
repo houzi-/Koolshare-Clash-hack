9ff57076d8201cccf46734dd4edb02e3a5fbcfa7
houzi- <wojiaolinmu008@gmail.com>
2022-06-25 18:01:26 +0800

    Update version <0.17.9>

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"koolclash",
-"version":"0.17.9-beta",
-"md5":"674684f6b2c81c4ab2a1979904ed7a3c",
+"version":"0.17.9",
+"md5":"5efdcdc31b70f5c98c9996205be6ba47",
 "home_url":"Module_koolclash.asp",
 "title":"koolclash",
 "description":"基于规则的代理程序 Clash",
 "changelog":"更新修复",
-"build_date":"2022-06-19_11:20:03"
+"build_date":"2022-06-25_17:59:53"
 }
