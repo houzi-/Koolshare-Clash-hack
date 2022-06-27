f9323a6935dab031d809e89ec99a746c742d9a59
houzi <wojiaolinmu008@gmail.com>
2022-06-27 11:29:27 +0800

    Refine

--- koolclash/scripts/koolclash_save_config.sh
+++ koolclash/scripts/koolclash_save_config.sh
@@ -18,7 +18,7 @@ if [ -f /tmp/upload/clash.config.yaml ]; then
     mkdir -p $KSROOT/koolclash/config/
     # 将上传的文件复制到 Config 目录中
     cp /tmp/upload/clash.config.yaml $KSROOT/koolclash/config/origin.yml
-	sub_time=\$(date +%Y-%m-%d\ %X)
+	sub_time=$(date +%Y-%m-%d\ %X)
 	dbus set koolclash_config_version="<font color="#1bbf35">$sub_time</font>"	
 else
     echo_date "没有找到上传的配置文件！退出！"
