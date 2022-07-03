5da3aea565447775faf1fd7a0c1d0b27509c69fd
houzi- <wojiaolinmu008@gmail.com>
2022-07-03 19:28:31 +0800

    Update version <0.18.0-beta>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ VERSION=`cat ./koolclash/webs/res/koolclash_.version`
 TITLE=koolclash
 DESCRIPTION="基于规则的代理程序 Clash"
 HOME_URL="Module_koolclash.asp"
-CHANGELOG="更新修复"
+CHANGELOG="修复BUG"
 
 # Check and include base
 DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"koolclash",
 "version":"0.17.9",
-"md5":"5efdcdc31b70f5c98c9996205be6ba47",
+"md5":"fed06211579c37dc634f277342ffe832",
 "home_url":"Module_koolclash.asp",
 "title":"koolclash",
 "description":"基于规则的代理程序 Clash",
 "changelog":"更新修复",
-"build_date":"2022-06-25_17:59:53"
+"build_date":"2022-07-03_19:27:30"
 }
