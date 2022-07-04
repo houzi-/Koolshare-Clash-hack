0bdf4df000b672efd3264242ab2dc9d02a18fe51
houzi <wojiaolinmu008@gmail.com>
2022-07-04 11:33:40 +0800

    Update script
    The proxy mode is changed from indirect switching to real-time switching.<koolclash_switch_direct.sh、koolclash_switch_global.sh、koolclash_switch_rule.sh>

--- koolclash/scripts/koolclash_switch_global.sh
+++ koolclash/scripts/koolclash_switch_global.sh
@@ -6,6 +6,18 @@ alias echo_date='echo 【$(date +%Y年%m月%d日\ %X)】:'
 
 eval $(dbus export koolclash_)
 
+#提取 Clash 配置认证码
+secret=$(yq r $KSROOT/koolclash/config/config.yaml secret)
+#提取 Clash 配置监听端口和 IP 地址
+ipaddr_port=$(yq r $KSROOT/koolclash/config/config.yaml external-controller)
+
+curl -sv \
+-H "Authorization: Bearer $secret" \
+-X PATCH "http://$ipaddr_port/configs/"  -d "{\"mode\": \"global\"}" 2>&1
+
+curl -sv \
+-H "Authorization: Bearer $secret" \
+-X DELETE "http://$ipaddr_port/connections"  2>&1
 
 dbus set koolclash_switch_config_mode=2
 yq w -i $KSROOT/koolclash/config/config.yaml mode "global"
