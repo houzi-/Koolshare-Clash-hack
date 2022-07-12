6fabc1b2929a5edd2de586d672fbdcc120227a4e
houzi <wojiaolinmu008@gmail.com>
2022-07-12 10:08:48 +0800

    Refine script

--- koolclash/scripts/koolclash_flow_status.sh
+++ koolclash/scripts/koolclash_flow_status.sh
@@ -13,14 +13,14 @@ if [ "$koolclash_update_mode" == "2" ]; then
     then
         total=$(cat /tmp/header.txt | awk '{printf $4}' | sed -e 's/total=//g' -e 's/;//g')
         down=$(cat /tmp/header.txt | awk '{printf $3}' | sed -e 's/download=//g' -e 's/;//g')
-	    time=$(cat /tmp/header.txt | awk '{printf $5}' | sed -e 's/expire=//g' -e 's/;//g')
+        time=$(cat /tmp/header.txt | awk '{printf $5}' | sed -e 's/expire=//g' -e 's/;//g')
         val_1=$(expr $total / 1073741824)
         val_2=$(expr $down / 1073741824)
         TOTAL=$(echo $val_1)
         USED=$(echo $val_2)
-	    expiration_time=$(date -d @$time '+%Y-%m-%d')
+        expiration_time=$(date -d @$time '+%Y-%m-%d')
         dbus set koolclash_sub_expiration_time="<font color=#1bbf35>$expiration_time</font>"
-		dbus set koolclash_sub_information="show"
+        dbus set koolclash_sub_information="show"
         http_response "$TOTAL>$USED"
     fi
 else
