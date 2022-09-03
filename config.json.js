bbb26f14e24d5c71ca97f0c7d2e63b826e03c052
houzi- <wojiaolinmu008@gmail.com>
2022-09-03 15:56:08 +0800

    Refine version v0.18.2

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"koolclash",
 "version":"0.18.2",
-"md5":"56d35c6bed14d2d26a6c30580aff20a0",
+"md5":"04cf28c0d52b5aa6d48025e1ce403028",
 "home_url":"Module_koolclash.asp",
 "title":"koolclash",
 "description":"基于规则的代理程序 Clash",
 "changelog":"更新修复",
-"build_date":"2022-08-27_17:56:54"
+"build_date":"2022-09-03_15:54:50"
 }
