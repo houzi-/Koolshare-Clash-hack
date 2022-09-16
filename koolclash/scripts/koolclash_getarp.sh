3d28cb058c6e2f1d98d56ea2eec0d20e022f570f
houzi <wojiaolinmu008@gmail.com>
2022-09-16 23:06:52 +0800

    Refine get_arp

--- koolclash/scripts/koolclash_getarp.sh
+++ koolclash/scripts/koolclash_getarp.sh
@@ -5,7 +5,7 @@ source $KSROOT/scripts/base.sh
 #arp=`arp | grep br-lan | grep -v ? |sed 's/<incomplete>/ /g'| sed 's/ (/</g'|sed 's/) at /</'|cut -d " " -f1|sed ':a;N;$!ba;s#\n#>#g'`
 #arp=`arp -i br-lan | grep 192. | sed 's/?/unknown-device.lan/g' | grep -v '<incomplete>' | sed 's/ (/</g'|sed 's/) at /</' | cut -d " " -f1 | awk '{print FNR " " $0}' | sed 's/ /-/g' | sed ':a;N;$!ba;s#\n#>#g'`
 arp_scan=$(which arp-scan)
-$arp_scan --interface=br-lan -l | grep "(Unknown" | sed 's/(Unknown.*/(Unknown)/'> /tmp/list.txt
+$arp_scan --interface=br-lan -l | grep "(Unknown" | sed 's/(Unknown.*/(Unknown)/' > /tmp/list.txt
 
 arp=$(cat /tmp/list.txt | awk '{$3 = $2; print $0}' | awk '{$2 = $1; print $0}' | sed 's/ /</g' | sed ':a;N;$!ba;s#\n#>#g')
 
